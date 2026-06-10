61d2c099d086014a0c0f073da809db066e5403d3
GRM458 <gr3509003@gmail.com>
2026-06-10 05:56:15 -0700

    my second commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Aos from 'aos';
+export default function Header() {
+  return (
+    <>
+    <div className='h-[200vh]'>helow</div>
+    <div className='w-full flex justify-center'>
+
+   <div data-aos="fade-right">game of Death</div>
+    </div>
+    </>
+  )
+}
